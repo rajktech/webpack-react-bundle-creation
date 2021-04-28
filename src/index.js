8d82ce95464bd4f88e1e6ba7757107d2899112d7
Raj Keshwani <raj.ignoumca@gmail.com>
2021-04-28 18:19:36 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+
+
+const App = () => {
+  return (
+    <div>
+        <div>Welcome to my-webpack-react-starter11</div>
+        
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
